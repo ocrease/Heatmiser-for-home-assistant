5ce3d936a7b2d6c066feea814ed0fa7560d02c25
Oliver Crease <18347739+ocrease@users.noreply.github.com>
2025-12-10 00:18:36 +0000

    Added more tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,18 @@
 """Define fixtures for AirNow tests."""
 
-from typing import Any
-
-from custom_components.heatmiserneo.const import DOMAIN
+from custom_components.heatmiserneo.const import (
+    CONF_DEFAULTS,
+    CONF_HVAC_MODES,
+    CONF_STAT_HOLD_DURATION,
+    CONF_STAT_HOLD_TEMP,
+    CONF_STAT_MAX_TEMPERATURE,
+    CONF_STAT_MIN_TEMPERATURE,
+    CONF_THERMOSTAT_OPTIONS,
+    CONF_TIMER_HOLD_DURATION,
+    CONF_TIMER_OPTIONS,
+    DOMAIN,
+    AvailableMode,
+)
 import pytest
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
@@ -17,9 +27,25 @@ def auto_enable_custom_integrations(enable_custom_integrations):
 
 @pytest.fixture(name="config_entry")
 def config_entry_fixture(
-    hass: HomeAssistant, config: dict[str, Any], options: dict[str, Any]
+    hass: HomeAssistant,  # , config: dict[str, Any], options: dict[str, Any]
 ) -> MockConfigEntry:
     """Mock a config entry for the integration."""
-    entry = MockConfigEntry(domain=DOMAIN, data={"host": "test-hub", "port": 1234})
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={"host": "test-hub", "port": 1234},
+        options={
+            CONF_DEFAULTS: {
+                CONF_THERMOSTAT_OPTIONS: {
+                    CONF_STAT_HOLD_TEMP: 1,
+                    CONF_STAT_HOLD_DURATION: 15,
+                    CONF_STAT_MIN_TEMPERATURE: 5.0,
+                    CONF_STAT_MAX_TEMPERATURE: 20.0,
+                },
+                CONF_TIMER_OPTIONS: {CONF_TIMER_HOLD_DURATION: 17},
+            },
+            CONF_HVAC_MODES: {"TEST_HC": {AvailableMode.HEAT}},
+        },
+    )
+    # entry.runtime_data = HeatmiserNeoData(AsyncMock(), AsyncMock())
     entry.add_to_hass(hass)
     return entry
